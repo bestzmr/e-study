4e2cab8ec72036412c2a1ba90b4cf73d2cececb3
merlin <z2485782203@163.com>
2019-12-30 14:53:40 +0800

    增加登录，注册功能

--- src/main/java/cn/bestsort/e_study/controller/UserController.java
+++ src/main/java/cn/bestsort/e_study/controller/UserController.java
@@ -1,8 +1,10 @@
 package cn.bestsort.e_study.controller;
 
-import cn.bestsort.e_study.pojo.entity.User;
+import cn.bestsort.e_study.pojo.dto.User;
+import cn.bestsort.e_study.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -13,7 +15,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 @Api(tags = "用户相关接口")
 public class UserController {
-
+    @Autowired
+    private UserService userService;
     @ApiOperation(value = "新增用户",notes = "这里是备注信息")
     @PostMapping("/add")
     public boolean addUser(@RequestBody User user) {
@@ -36,5 +39,34 @@ public class UserController {
     @DeleteMapping("/delete/{id}")
     public boolean delete(@PathVariable("id") int id) {
         return true;
+    }
+
+
+    @ApiOperation(value = "用户注册")
+    @PostMapping("/register")
+    public Boolean register(@RequestParam User user){
+        if (userService.isUserExist(user.getTel())){
+            //该用户已存在
+            return false;
+        }else{
+            //用户不存在，添加该用户
+            boolean result = userService.addUser(user);
+            if (result){
+                //注册成功，返回登录页面
+                return true;
+            }else{
+                //注册失败，请重新输入
+                return false;
+            }
+        }
+    }
+
+    @ApiOperation(value = "用户登录")
+    @GetMapping("/login")
+    public Boolean login(@RequestParam User user){
+        if (userService.login(user)){
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/cn/bestsort/e_study/service/UserService.java
+++ src/main/java/cn/bestsort/e_study/service/UserService.java
@@ -1,8 +1,45 @@
 package cn.bestsort.e_study.service;
 
+import cn.bestsort.e_study.mapper.UserMapper;
+import cn.bestsort.e_study.pojo.dto.User;
+import cn.bestsort.e_study.pojo.dto.UserExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * @Author zhaoqiang
- * @Date 2019/12/30 8:54
+ * @Date 2019/12/30 14:08
  */
-public interface UserService {
+@Service
+public class UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    public Boolean isUserExist(String tel){
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andTelEqualTo(String.valueOf(tel));
+        List<User> userlist = userMapper.selectByExample(userExample);
+        if (userlist == null){
+            return false;
+        }
+        return true;
+    }
+    public Boolean addUser(User user){
+        int result = userMapper.insertSelective(user);
+        if (result == 1){
+            return true;
+        }
+        return false;
+    }
+    public Boolean login(User user){
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andTelEqualTo(user.getTel()).andPasswordEqualTo(user.getPassword());
+        List<User> userList = userMapper.selectByExample(userExample);
+        if (userList==null) {
+            return false;
+        }
+        return true;
+    }
 }
